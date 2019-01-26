1fa91c2c7044455923d337f778dbcce196912feb
oleg <oleb140888@gmail.com>
2019-01-27 00:38:21 +0200

    Updates

---
+++ precache-manifest.b197c7ce1caa5948d80fb6aec398f04a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "35e058ed00804edf4a7d",
+    "url": "/task-list-app/static/js/runtime~main.35e058ed.js"
+  },
+  {
+    "revision": "a1d962763ac5300b5d20",
+    "url": "/task-list-app/static/js/main.a1d96276.chunk.js"
+  },
+  {
+    "revision": "d1407d3014fdcd8b857f",
+    "url": "/task-list-app/static/js/1.d1407d30.chunk.js"
+  },
+  {
+    "revision": "a1d962763ac5300b5d20",
+    "url": "/task-list-app/static/css/main.e8ae8ce3.chunk.css"
+  },
+  {
+    "revision": "7fdcd84e0a1448619b289c932c75366d",
+    "url": "/task-list-app/index.html"
+  }
+];
